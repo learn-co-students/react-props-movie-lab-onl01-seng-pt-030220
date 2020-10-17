f7e054f778ec568d835d1b7bf7cba43422365dd0
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-17 11:26:10 -0400

    Done.

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 export default class CardFront extends Component {
-
+//CardFront will only be used to display the movie poster. 
   render() {
+    console.log(this.props, "this is the card front props")
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
       </div>
     )
   }
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -28,16 +28,24 @@ const posterMap = {
 }
 
 export default class MovieCard extends Component {
+  // title, IMDBRating, genres, and poster.
 
   render() {
+    console.log(this.props, "Props are here")
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront poster={posterMap[this.props.poster]} />
+        <CardBack title={this.props.title} genres={this.props.genres} IMDBRating={this.props.IMDBRating} />
       </div>
     )
   }
 }
 
 // Don't forget your default props!
+MovieCard.defaultProps = {
+  poster: 'default',
+  title: 'Unknown',
+  IMDBRating: null, 
+  genres: ['No Genre(s) Found']
+}
